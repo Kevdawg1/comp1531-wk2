6fb0eaacfc773dbb2bdb9e38318e55b327354f85
Kevdawg1 <z5076319@cse.unsw.edu.au>
2017-08-03 12:40:48 +1000

    fizzbuzz100

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 #!/bin/python3.5
-import sys
-n = int (sys.argv[1])
-strOut = ""
-if (n % 3 == 0):
-	strOut = strOut + "Fizz "
-if (n % 5 == 0):
-	strOut = strOut + "Buzz"
-if (strOut == ""):
-	print (n)
-else:	
-	print (strOut)
+#import sys
+for n in range(101):
+	
+	#n = int (sys.argv[1])
+	strOut = ""
+	if (n % 3 == 0):
+		strOut = strOut + "Fizz "
+	if (n % 5 == 0):
+		strOut = strOut + "Buzz"
+	if (strOut == ""):
+		print (n)
+	else:	
+		print (strOut)
