c08724464b81b4a70b1abf85dbaef39356c8a894
Kevdawg1 <z5076319@cse.unsw.edu.au>
2017-08-03 13:00:14 +1000

    made into function

--- hundredfb.py
+++ hundredfb.py
@@ -1,15 +1,7 @@
 #!/bin/python3.5
-#import sys
-import FizzBuzz()
-def hundred-fb():
+import sys
+from fizzbuzz import FizzBuzz
+def hundredfb():
 	for n in range(101):
-		strOut = ""
-		if (n % 3 == 0):
-			strOut = strOut + "Fizz "
-		if (n % 5 == 0):
-			strOut = strOut + "Buzz"
-		if (strOut == ""):
-			print (n)
-		else:	
-			print (strOut)
-hundred-fb()
+		FizzBuzz(n)
+hundredfb()
